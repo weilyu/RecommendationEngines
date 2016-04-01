ff566aefefc87cde83acdb7d48213cabbf3040f5
Wei Lyu <lvwei1001@live.com>
2016-04-01 15:31:22 +0800

    printAverageRatings

--- src/MovieRunnerAverage.java
+++ src/MovieRunnerAverage.java
@@ -2,4 +2,10 @@
  * Created by lvwei on 4/1/2016.
  */
 public class MovieRunnerAverage {
+    public void printAverageRatings() {
+        SecondRatings sr = new SecondRatings("ratedmovies_short.csv", "ratings_short.csv");
+        System.out.println("The number of movies is " + sr.getMovieSize());
+        System.out.println("The number of raters is " + sr.getRaterSize());
+
+    }
 }
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lvwei on 4/1/2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        MovieRunnerAverage mra = new MovieRunnerAverage();
+        mra.printAverageRatings();
+    }
+}
