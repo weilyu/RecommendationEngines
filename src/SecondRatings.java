ed9af561b8dcf2a25ea50aed5bfaba23e5555e15
Wei Lyu <lvwei1001@live.com>
2016-04-01 15:19:33 +0800

    done constructor

--- src/SecondRatings.java
+++ src/SecondRatings.java
@@ -1,8 +1,8 @@
 
 /**
  * Write a description of SecondRatings here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 
@@ -11,10 +11,16 @@ import java.util.*;
 public class SecondRatings {
     private ArrayList<Movie> myMovies;
     private ArrayList<Rater> myRaters;
-    
+
     public SecondRatings() {
         // default constructor
         this("ratedmoviesfull.csv", "ratings.csv");
     }
-    
+
+    public SecondRatings(String movieFile, String ratingsFile) {
+        FirstRatings fr = new FirstRatings();
+        myMovies = fr.loadMovies(movieFile);
+        myRaters = fr.loadRaters(ratingsFile);
+    }
+
 }
