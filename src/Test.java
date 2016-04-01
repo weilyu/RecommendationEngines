4ffbd7ea7e7c1716b718f254aba658bb6d2b0f57
Wei Lyu <lvwei1001@live.com>
2016-04-01 13:33:26 +0800

    change the name of test class

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 /**
  * Created by lvwei on 3/9/2016.
  */
-public class Main {
+public class Test {
     public static void main(String[] args) {
         FirstRatings frt = new FirstRatings();
         frt.testLoadMovies();
