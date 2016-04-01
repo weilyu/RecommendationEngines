cd454afa19c3640e1e56d5bc10dda5e4d17e3011
Wei Lyu <lvwei1001@live.com>
2016-04-01 16:10:27 +0800

    printAverageRatings

--- src/MovieRunnerAverage.java
+++ src/MovieRunnerAverage.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by lvwei on 4/1/2016.
  */
@@ -7,5 +9,13 @@ public class MovieRunnerAverage {
         System.out.println("The number of movies is " + sr.getMovieSize());
         System.out.println("The number of raters is " + sr.getRaterSize());
 
+        //print a list of movies and their average ratings
+        //for all those movies that have at least a
+        //specified number of ratings, sorted by averages, lowest rating to highest rating
+        //one movie per line (print its rating first, followed by its title)
+        ArrayList<Rating> averageRatings =  sr.getAverageRatings(3);
+        for (Rating r : averageRatings) {
+            System.out.println(r.getValue() + " " + sr.getTitle(r.getItem()));
+        }
     }
 }
