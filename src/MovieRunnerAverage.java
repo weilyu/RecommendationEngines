133c61f3af6a3a3c69ad5d523984c2bba5f7b2fa
Wei Lyu <lvwei1001@live.com>
2016-04-02 14:47:37 +0800

    sort averageRatings, add getAverageRatingOneMovie method

--- src/MovieRunnerAverage.java
+++ src/MovieRunnerAverage.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Created by lvwei on 4/1/2016.
@@ -14,8 +16,20 @@ public class MovieRunnerAverage {
         //specified number of ratings, sorted by averages, lowest rating to highest rating
         //one movie per line (print its rating first, followed by its title)
         ArrayList<Rating> averageRatings =  sr.getAverageRatings(3);
+        Collections.sort(averageRatings);
         for (Rating r : averageRatings) {
             System.out.println(r.getValue() + " " + sr.getTitle(r.getItem()));
         }
+        System.out.println("\n");
+    }
+
+    //first create a SecondRatings object, reading in data from the movie and ratings data files
+    //print out the average ratings for a specific movie title
+    public void getAverageRatingOneMovie() {
+        SecondRatings sr = new SecondRatings("ratedmovies_short.csv", "ratings_short.csv");
+        String movieToFind = "The Godfather"; //change this to find another movie
+        String curID = sr.getID(movieToFind);
+        double averageRating = sr.getAverageByID(curID, 0);
+        System.out.println("The average rating of " + movieToFind + " is " + averageRating);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         MovieRunnerAverage mra = new MovieRunnerAverage();
         mra.printAverageRatings();
+        mra.getAverageRatingOneMovie();
     }
 }
