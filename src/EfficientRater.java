22e5ca73aea39cceaa3cd2dd814ddcef1ea9aa48
Wei Lyu <lvwei1001@live.com>
2016-04-03 17:59:46 +0800

    1.5 EfficientRater

---
+++ src/EfficientRater.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by lvwei on 4/3/2016.
+ */
+public class EfficientRater implements Rater {
+    private String myID;
+    private HashMap<String, Rating> myRatings; //movie id, Rating associated with this movie
+
+    public EfficientRater(String id) {
+        myID = id;
+        myRatings = new HashMap<>();
+    }
+
+    public void addRating(String item, double rating) {
+        myRatings.put(item, new Rating(item, rating));
+    }
+
+    //return true if this item is in myRatings, and false otherwise
+    public boolean hasRating(String item) {
+        if (myRatings.containsKey(item)) return true;
+        return false;
+    }
+
+    public String getID() {
+        return myID;
+    }
+
+    //return the double rating of this item if it is in myRatings, return -1 if not found
+    public double getRating(String item) {
+        if (hasRating(item)) return myRatings.get(item).getValue();
+        return -1;
+    }
+
+    public int numRatings() {
+        return myRatings.size();
+    }
+
+    //return a list of all the items that have been rated
+    public ArrayList<String> getItemsRated() {
+        ArrayList<String> list = (ArrayList<String>) myRatings.keySet();
+        return list;
+    }
+}
