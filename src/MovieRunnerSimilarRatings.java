d574bacc1be20ea7b2a79294b791d2ba3194c684
Wei Lyu <lvwei1001@live.com>
2016-04-04 17:27:29 +0800

    MovieRunnerSimilarRatings

--- src/MovieRunnerSimilarRatings.java
+++ src/MovieRunnerSimilarRatings.java
@@ -1,5 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by lvwei on 4/4/2016.
  */
 public class MovieRunnerSimilarRatings {
+    public void printAverageRatings() {
+        FourthRatings sr = new FourthRatings("ratings.csv");
+
+        MovieDatabase.initialize("ratedmoviesfull.csv");
+        RaterDatabase.initialize("ratings.csv");
+
+        System.out.println("read data for " + RaterDatabase.size() + " raters");
+
+        System.out.println("read data for " + MovieDatabase.size() + " movies");
+
+        int minimalRaters = 35;
+
+        //print a list of movies and their average ratings
+        //for all those movies that have at least a
+        //specified number of ratings, sorted by averages, lowest rating to highest rating
+        //one movie per line (print its rating first, followed by its title)
+        ArrayList<Rating> averageRatings = sr.getAverageRatings(minimalRaters);
+        System.out.println("found " + averageRatings.size() + " movies");
+
+        Collections.sort(averageRatings);
+        for (Rating r : averageRatings) {
+            System.out.println(r.getValue() + " " + MovieDatabase.getTitle(r.getItem()));
+        }
+        System.out.println("\n");
+    }
+
+    public void printAverageRatingsByYearAfterAndGenre() {
+        FourthRatings sr = new FourthRatings("ratings.csv");
+
+        MovieDatabase.initialize("ratedmoviesfull.csv");
+        RaterDatabase.initialize("ratings.csv");
+
+        System.out.println("read data for " + RaterDatabase.size() + " raters");
+
+        System.out.println("read data for " + MovieDatabase.size() + " movies");
+
+        int minimalRaters = 8;
+
+        AllFilters f = new AllFilters();
+        f.addFilter(new YearAfterFilter(1990));
+        f.addFilter(new GenreFilter("Drama"));
+
+        ArrayList<Rating> averageRatings = sr.getAverageRatingsByFilter(minimalRaters, f);
+        System.out.println("found " + averageRatings.size() + " movies");
+
+        Collections.sort(averageRatings);
+        for (Rating r : averageRatings) {
+            System.out.println(r.getValue() + " " + MovieDatabase.getYear(r.getItem()) + " " + MovieDatabase.getTitle(r.getItem()) + " " + MovieDatabase.getDirector(r.getItem()));
+        }
+        System.out.println("\n");
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -3,29 +3,8 @@
  */
 public class Test {
     public static void main(String[] args) {
-//        MovieRunnerAverage mra = new MovieRunnerAverage();
-//        mra.printAverageRatings();
-//        mra.getAverageRatingOneMovie();
-        MovieRunnerWithFilters mrwf = new MovieRunnerWithFilters();
-        System.out.println("printAverageRatings");
-        mrwf.printAverageRatings();
-
-        System.out.println("printAverageRatingsByYear");
-        mrwf.printAverageRatingsByYear();
-
-        System.out.println("printAverageRatingsByGenre");
-        mrwf.printAverageRatingsByGenre();
-
-        System.out.println("printAverageRatingsByMinutes");
-        mrwf.printAverageRatingsByMinutes();
-
-        System.out.println("printAverageRatingsByDirectors");
-        mrwf.printAverageRatingsByDirectors();
-
-        System.out.println("printAverageRatingsByYearAfterAndGenre");
-        mrwf.printAverageRatingsByYearAfterAndGenre();
-
-        System.out.println("printAverageRatingsByDirectorsAndMinutes");
-        mrwf.printAverageRatingsByDirectorsAndMinutes();
+        MovieRunnerSimilarRatings mr = new MovieRunnerSimilarRatings();
+        mr.printAverageRatings();
+        mr.printAverageRatingsByYearAfterAndGenre();
     }
 }
